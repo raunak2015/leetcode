78358971ae8202f8d41c39d9123887fd5942c5fd
Raunak Shahu <raunakshahu.cg@gmail.com>
2026-07-23 15:10:00 +0530

    Time: 7 ms (91.3%), Space: 13.7 MB (37.96%) - LeetHub

---
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        for(int a = 0; a<n-3;a++){
+            for(int b = a+1; b<n-2; b++){
+                for(int c = b+1; c<n-1;c++){
+                    for(int d = c+1; d<n; d++){
+                        if(nums[a]+nums[b]+nums[c] == nums[d])
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
