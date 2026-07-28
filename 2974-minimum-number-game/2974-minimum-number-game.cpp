cc7aeb4e73c1cf684952a64aa9ef647ac9028e71
Raunak Shahu <raunakshahu.cg@gmail.com>
2026-07-28 14:45:53 +0530

    Time: 0 ms (100%), Space: 28.1 MB (93.29%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> ans;
-        for(int i = 1;i<nums.size();i+=2){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i-1]);
+        for(int i = 0;i<nums.size();i+=2){
+            swap(nums[i], nums[i + 1]);
         }
-        return ans;
+        return nums;
     }
 };
