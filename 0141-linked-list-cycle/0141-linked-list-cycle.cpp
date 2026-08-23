c3ae5a6229e3d066b15a166146479efeccc1a906
Raunak Shahu <raunakshahu.cg@gmail.com>
2026-08-23 23:09:04 +0530

    Time: 8 ms (81.79%), Space: 11.7 MB (97.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -7,18 +7,19 @@
  * };
  */
 class Solution {
+
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycle(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast && fast->next){
+        while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
-
-            if(slow == fast)
-            return true;
+            if (slow == fast)
+                return true;
         }
+
         return false;
     }
 };
