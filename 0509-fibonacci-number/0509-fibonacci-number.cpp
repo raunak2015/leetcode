cc97ca1addd9fdb057034b9ae003aeed73fb7f44
Raunak Shahu <raunakshahu.cg@gmail.com>
2026-09-06 12:50:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.37%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = n;
+        for(int i = 2; i<=n;i++){
+            n3 = n1+n2;
+            n1= n2;
+            n2= n3;
+        }
+        return n3;
+    }
+};
